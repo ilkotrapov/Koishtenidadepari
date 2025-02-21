0400215a926e3eaba8b066d5ac1e10d748041d45
Kalin-KO <kalin.ognyanov@proton.me>
2025-02-21 20:48:59 +0200

    Putting in a branch the barebones of the payment integration. Not complete

---
+++ Delivery System (Team Enif)/Delivery System (Team Enif)/Models/Stripe/StripeSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Delivery_System__Team_Enif_.Models.Stripe
+{
+    public class StripeSettings
+    {
+        public string SecretKey { get; set; }
+        public string PublishableKey { get; set; }
+        public string WebhookSecret { get; set; }
+    }
+}
